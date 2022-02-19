010fb5b243509ef221b7a78bd42a767ed5935a38
codamir <co.damir@gmail.com>
2022-02-19 16:46:53 +0100

    Task 9 update

--- Ninth_task.java
+++ Ninth_task.java
@@ -2,19 +2,15 @@ public class Ninth_task {
     public static void main(String[] args) {
         int[] firstArray = {1,2,3};
         int[] secondArray = {1,2,3};
-        int[] sumArray = new int[firstArray.length];
 
-        int sumFirst = 0;
-        int sumSecond =0;
+        int sum=0;
 
-        for (int i=0;i<firstArray.length;i++) {
-            sumArray[i] = firstArray[i] + sumFirst;
-            for (int j=0;j < secondArray.length;j++){
-                sumArray[j] = secondArray[j] + sumSecond;
-                int sum = sumArray[i]+sumArray[j];
-                System.out.println(sum);
+        for(int i=0;i<firstArray.length;i++){
+            for(int j=0;j<secondArray.length;j++){
+                sum = firstArray[i] + secondArray[j]+sum;
             }
         }
+        System.out.println("Total sum of two arrays: "+sum);
     }
 }
 
@@ -22,3 +18,4 @@ public class Ninth_task {
 //    Napravite program koji ce izracunati total kada se svaki element iz jednog niza
 //    sabere sa svakim elementom iz drugog niza.npr:[1,2,3]
 //    [1,2,3] Total sabiranja: 36
+
